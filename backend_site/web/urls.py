aebff8575234ca7ff657973053bb1c4709dcf411
waterbulca <waterbulca@gmail.com>
2021-06-13 11:08:53 +0300

    написалп простой get запрос для тестирования

--- backend_site/web/urls.py
+++ backend_site/web/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.CarView),
+    path('api/cars/', views.CarView.as_view()),
 ]
--- backend_site/web/views.py
+++ backend_site/web/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from django.http import HttpResponse
+import logging
 from .models import Car
 from .serializers import CarSerializer
+from django.shortcuts import render
+
 # Create your views here.
 
-def CarView(APIView):
+logger = logging.getLogger(__name__)
+
+class CarView(APIView):
 
     def get(self, request):
-        cars = Car.objects
-        serializer = CarSerializer(cars,many=True)
-        return Response(serializer.data)
+        cars = Car.objects.all()
+        serializer = CarSerializer(instance=cars,many=True)
+        resp = Response(serializer.data)
+        if Response(serializer.data)==None:
+            resp = {"status" : "none"}
+        return resp
